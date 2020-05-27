c6fbbd53c40d86dab5a9f82cd92fe54b54eb4b01
Marissa Amaya <mamaya.dev@gmail.com>
2020-05-27 13:48:33 -0500

    connect to mongo

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,21 @@
 import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
 
 import schema from './schema';
 
 const server = new ApolloServer({ schema });
+
+mongoose.connect(
+  'mongodb://localhost:27017/billed',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      return console.log('✘ Error connecting to MongoDB:', err);
+    }
+
+    console.log('✔ Successfully connected to MongoDB');
+  },
+);
 
 server
   .listen({ port: 4200 })
